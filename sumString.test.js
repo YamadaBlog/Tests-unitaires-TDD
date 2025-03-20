f52878e2303f5711506c60f90fa9cf704d69f7e0
YamadaBlog <yamadaablog@gmail.com>
2025-03-20 11:39:51 +0100

    Ajout de node_modules au .gitignore

---
+++ sumString.test.js
@@ -0,0 +1,14 @@
+try {
+  console.assert(
+    sumString("2,3") === 5,
+    '❌ sumString("2,3") doit retourner 5'
+  );
+  console.assert(
+    sumString("0.1,0.2") === 0.3,
+    '❌ sumString("0.1,0.2") doit retourner 0.3'
+  );
+
+  console.log("Tous les tests passent sont OK !");
+} catch (error) {
+  console.error(error.message);
+}
